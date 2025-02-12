9c1e071e4981a61835ac830eff23b2707001acd1
afridi00000 <afridi@usf.edu>
2025-02-12 17:26:35 -0500

    Task 1 - Employee Salary Calculation

---
+++ cc_8.js
@@ -0,0 +1,8 @@
+// Task 1: Function Declaration - Employee Salary Calculation
+function calculateSalary(baseSalary, bonus, taxRate) {
+    let netSalary = (baseSalary + bonus) - (baseSalary * taxRate);
+    return `Net Salary: $${netSalary.toFixed(2)}`;
+}
+
+console.log(calculateSalary(5000, 500, 0.1)); 
+console.log(calculateSalary(7000, 1000, 0.15)); 
