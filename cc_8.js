f8d06e727c80769da4279f5d15da267d24072b9f
afridi00000 <afridi@usf.edu>
2025-02-12 17:27:16 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -6,3 +6,11 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 
 console.log(calculateSalary(5000, 500, 0.1)); 
 console.log(calculateSalary(7000, 1000, 0.15)); 
+// Task 2: Function Expression - Product Price After Discount
+const calculateDiscount = function(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+};
+
+console.log(calculateDiscount(100, 0.2)); 
+console.log(calculateDiscount(250, 0.15)); 
