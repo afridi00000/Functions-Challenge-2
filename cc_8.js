271b91fb3451dfe6b6a3a612c137f479942282ad
afridi00000 <afridi@usf.edu>
2025-02-12 17:28:31 -0500

    Task 5 - Loan Payment Calculation

--- cc_8.js
+++ cc_8.js
@@ -42,3 +42,11 @@ function calculateRentalCost(days, carType, insurance = false) {
 
 console.log(calculateRentalCost(3, "Economy", true)); 
 console.log(calculateRentalCost(5, "Luxury", false)); 
+// Task 5: Returning Values - Loan Payment Calculation
+function calculateLoanPayment(principal, rate, time) {
+    let totalPayment = principal + (principal * rate * time);
+    return `Total Payment: $${totalPayment.toFixed(2)}`;
+}
+
+console.log(calculateLoanPayment(1000, 0.05, 2)); 
+console.log(calculateLoanPayment(5000, 0.07, 3)); 
